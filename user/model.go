cc5d5a3bedd70400de3544e5759228de5c4a8e5f
KaguraiYoRoy <yorisocute233@gmail.com>
2026-01-24 21:49:50 +0800

    fix(user): Fix SQL Error: not null column with default value null when alerting users table

--- user/model.go
+++ user/model.go
@@ -7,10 +7,10 @@ import (
 // User 用户模型
 type User struct {
 	gorm.Model
-	Email        string `json:"email" gorm:"not null;size:100"`
+	Email        string `json:"email" gorm:"not null;default:'';size:100"`
 	Username     string `json:"username" gorm:"not null;unique;size:100"`
 	PasswordHash string `json:"-" gorm:"not null;size:255"`
-	Nickname     string `json:"nickname" gorm:"not null;size:100"`
+	Nickname     string `json:"nickname" gorm:"not null;default:'';size:100"`
 	Organization string `json:"organization" gorm:"size:100"`
 	Points       int    `json:"points" gorm:"default:0"`
 	Status       string `json:"status" gorm:"not null;default:'active';size:20"` // active, disabled
